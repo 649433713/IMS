66e79662fe5383b26db6ba7bf531b39c789920f9
yinywf <wangfan@superid.cn>
2019-04-24 15:44:28 +0800

    Merge branch 'master' of https://github.com/649433713/IMS
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ src/main/java/IMS/demo/dto/PurchaseGoodsDTO.java
@@ -0,0 +1,53 @@
+package IMS.demo.dto;
+
+import IMS.demo.dataobject.OrderDetailPO;
+import IMS.demo.dataobject.OrderMasterPO;
+import IMS.demo.dataobject.StockPurchasePO;
+import lombok.Data;
+
+@Data
+public class PurchaseGoodsDTO {
+    private String goodsId;
+    private Integer num;
+    private double price;
+    private double priceSum;
+    private double tax;
+    private String buyer;
+    private String note;
+
+    public PurchaseGoodsDTO(OrderDetailPO orderDetailPO, OrderMasterPO orderMasterPO) {
+        goodsId = orderDetailPO.getProductId();
+        num = orderDetailPO.getProductQuantity();
+        price = orderDetailPO.getProductPrice().doubleValue();
+        priceSum = num * priceSum;
+        tax = 0;
+        buyer = orderMasterPO.getBuyerName() + " " + orderMasterPO.getBuyerContact();
+        note = orderMasterPO.getOrderAbstract();
+    }
+
+    public PurchaseGoodsDTO(StockPurchasePO stockPurchasePO) {
+        goodsId = stockPurchasePO.getProductId();
+        num = stockPurchasePO.getQuantity();
+        price = stockPurchasePO.getUnitPrice();
+        priceSum = stockPurchasePO.getAmount();
+        tax = stockPurchasePO.getTax();
+        buyer = stockPurchasePO.getSeller();
+        note = stockPurchasePO.getComments();
+    }
+
+    public StockPurchasePO getPO() {
+        StockPurchasePO stockPurchasePO = new StockPurchasePO();
+        stockPurchasePO.setAmount(priceSum);
+        stockPurchasePO.setComments(note);
+        stockPurchasePO.setProductId(goodsId);
+        stockPurchasePO.setUnitPrice(price);
+        stockPurchasePO.setQuantity(num);
+        stockPurchasePO.setSeller(buyer);
+        stockPurchasePO.setTax(tax);
+        return stockPurchasePO;
+    }
+
+    public PurchaseGoodsDTO() {
+
+    }
+}
